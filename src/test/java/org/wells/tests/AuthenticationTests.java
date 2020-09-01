1657e1a48cb01354194924117050b81e77c33238
lostground97 <sethi.rishabh97@gmail.com>
2020-09-01 10:41:59 +0530

    Test File Added

--- src/test/java/org/wells/tests/AuthenticationTests.java
+++ src/test/java/org/wells/tests/AuthenticationTests.java
@@ -1,63 +1,41 @@
 package org.wells.tests;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.wells.controller.AuthenticationController;
-import org.wells.entity.AddUserRequest;
-import org.wells.service.AuthenticationService;
+import org.wells.models.AddUserRequest;
+import org.wells.models.User;
+import org.wells.services.AuthenticationService;
 
+@DataJpaTest
 @RunWith(SpringRunner.class)
-@WebMvcTest(value = AuthenticationController.class)
 public class AuthenticationTests {
 	
 	@Autowired
-	private MockMvc mockMvc;
+	private AddUserRequest UserTest;
 	
 	@MockBean
 	private AuthenticationService authenticationService;
 	
-	String exampleUser = "{\r\n" + 
-			"    \\\"username\\\": \\\"aditya\\\",\r\n" + 
-			"    \\\"firstName\\\": \\\"Aditya\\\",\r\n" + 
-			"    \\\"password\\\": \\\"1234\\\",\r\n" + 
-			"    \\\"lastName\\\": \\\"\\\",\r\n" + 
-			"    \\\"email\\\": \\\"aditya.99@gmail.com\\\",\r\n" + 
-			"    \\\"mobile\\\": \\\"7923632604\\\"\r\n" + 
-			"}";
-	
 	@Test
-	public void testCreateUser() throws Exception{
-		AddUserRequest mockUser = new AddUserRequest();
-		mockUser.setUsername("uname1");
-		mockUser.setFirstName("Fname1");
-		mockUser.setLastName("Lname");
-		mockUser.setConfirmed(true);
-		mockUser.setPassword("temp");;
-		mockUser.setEmail("temp@gmail.com");
-		mockUser.setMobile("123456789");
+	@Rollback(false)
+	public void testCreateUser() {
 		
-		RequestBuilder requestBuilder = MockMvcRequestBuilders
-				.post("/register")
-				.accept(MediaType.APPLICATION_JSON).content(exampleUser)
-				.contentType(MediaType.APPLICATION_JSON);
-		
-		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-		MockHttpServletResponse response = result.getResponse();
-		System.out.println(response.getStatus());
-		Assert.assertEquals(HttpStatus.CREATED.value(), response.getStatus());
-		
+			UserTest.setUsername("rishabh");
+			UserTest.setFirstName("Rishabh");;
+			UserTest.setLastName("Sethi");
+			UserTest.setPassword("password");
+			UserTest.setEmail("email");
+			UserTest.setMobile("789456123");
+			
+			User temp = authenticationService.save(UserTest);
+			assertThat(temp.getUsername()).isEqualTo("rishabh");
+			
 		}
 }
